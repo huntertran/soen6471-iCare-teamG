998c0f6e8c5aa87d4ac16bbdea065d63afa5e3e6
Tuan Tran Van <4468525+huntertran@users.noreply.github.com>
2020-05-19 12:26:17 -0400

    add sample Java API application using Spring framework

---
+++ src/test/java/soen6471/teamg/TeamgApplicationTests.java
@@ -0,0 +1,13 @@
+package soen6471.teamg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
